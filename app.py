d13cbebd9046ab2c9909b886293bffc161566579
nagoal <nagoal@gmail.com>
2025-01-27 20:37:16 -0500

    Update app.py
    Introducir número de intentos

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicacion aun no es funcional. En construccion.')
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+if start_button:
+  st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
