68bc49c319c97b1a909d461046fb7142354550bf
nagoal <nagoal@gmail.com>
2025-01-27 20:45:56 -0500

    Update app.py
    Introducir start button

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 st.header('Lanzar una moneda')
 
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
 if start_button:
   st.write(f'Experimento con {number_of_trials} intentos en curso.')
 
